6cb9b7e3f7d81e9f548b59d235548311c8f2ed3a
Sebastien Decuyper <s.decuyper314@gmail.com>
2020-12-21 23:55:12 +0100

    Push to see if I can change my website from GitKraken

---
+++ docs/firstJSscript.js
@@ -0,0 +1,29 @@
+let value1 = 5;
+let value2 = 10;
+
+function AddValues(x,y){
+
+    var result = x+y;
+}
+
+AddValues(value1, value2);
+
+function ChangeValue(){
+  document.getElementById("demo").innerHTML = "changed !";
+
+}
+
+
+
+
+/*let value1 = 5;
+let value2 = 10;
+
+function AddValues(x,y){
+
+    var res = x+y;
+}
+
+AddValues(value1, value2);
+
+console.log(res)*/
